a6ef4b0555afaa7f8bc2190e42527a0449675610
BenS99 <43781859+BenS99@users.noreply.github.com>
2019-04-16 11:49:18 +0100

    Add files via upload

---
+++ B00732896/B00732896/Crate/AppData.cpp
@@ -0,0 +1,17 @@
+#include "AppData.h"
+
+
+
+AppData::AppData()
+{
+	axisSelect = 'n';
+	reset = false;
+	frameType = 's';
+	cullMode = 'n';
+	cameraView = 0;
+}
+
+
+AppData::~AppData()
+{
+}
---
+++ B00732896/B00732896/Crate/AppData.h
@@ -0,0 +1,13 @@
+#pragma once
+class AppData
+{
+public:
+	AppData();
+	~AppData();
+	char axisSelect;
+	bool reset;
+	char frameType;
+	char cullMode;
+	int cameraView;
+};
+
